368d6515535eda7c6fe52b0306f9d2a5ec5a17ec
jieun-b <qwldms25@gmail.com>
2025-06-29 13:35:54 +0900

    Add problem

---
+++ 2169번 - 로봇 조종하기/main.py
@@ -0,0 +1,49 @@
+#  **************************************************************************  #
+#                                                                              #
+#                                                       :::    :::    :::      #
+#    Problem Number: 2169                              :+:    :+:      :+:     #
+#                                                     +:+    +:+        +:+    #
+#    By: jieunb_b <boj.kr/u/jieunb_b>                +#+    +#+          +#+   #
+#                                                   +#+      +#+        +#+    #
+#    https://boj.kr/2169                           #+#        #+#      #+#     #
+#    Solved: 2025/06/29 12:06:37 by jieunb_b      ###          ###   ##.kr     #
+#                                                                              #
+#  **************************************************************************  #
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+INF = -float('inf')
+
+dp = [[INF]*m for _ in range(n)]
+# 첫 줄
+for j in range(m):
+    if j == 0:
+        dp[0][j] = graph[0][j]
+    else:
+        dp[0][j] = dp[0][j-1] + graph[0][j]
+
+for i in range(1, n):
+    # 왼쪽
+    dp_left = [INF]*m
+    for j in range(m):
+        if j == 0:
+            dp_left[j] = dp[i-1][j] + graph[i][j]
+            continue
+        dp_left[j] = max(dp[i-1][j] + graph[i][j], dp_left[j-1] + graph[i][j])
+
+    # 오른쪽
+    dp_right = [INF]*m
+    for j in range(m-1, -1, -1):
+        if j == m-1:
+            dp_right[j] = dp[i-1][j] + graph[i][j]
+            continue
+        prev_i, prev_j = i, j+1
+        dp_right[j] = max(dp[i-1][j] + graph[i][j], dp_right[j+1] + graph[i][j])
+
+    for j in range(m):
+        dp[i][j] = max(dp_left[j], dp_right[j])
+print(dp[-1][-1])
